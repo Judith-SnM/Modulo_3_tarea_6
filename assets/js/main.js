7ec1a855acb10847f1a30184273c3ea5f5a024ad
Judith <judith.sanmartin.s@gmail.com>
2024-03-22 21:59:51 -0300

    correccion evaluacion

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,16 +10,11 @@ const radiologia = [
 //Punto 1 - se agregaron las horas//
 const traumatologia = [
     { hora: '8:00', especialista: 'MARIA PAZ ALTUZARRA', paciente: 'PAULA SANCHEZ', rut: '15554774-5', prevision: 'FONASA' },
-    { hora: '9:00', especialista: 'RENÉ POBLETE', paciente: 'ANA GELLONA', rut: '13123329-7', prevision: 'ISAPRE' },
-    { hora: '9:30', especialista: 'MARIA SOLAR', paciente: 'RAMIRO ANDRADE', rut: '12221451-K', prevision: 'FONASA' },
     { hora: '10:00', especialista: 'RAUL ARAYA', paciente: 'ANGÉLICA NAVAS', rut: '15444147-9', prevision: 'ISAPRE' },
-    { hora: '10:00', especialista: 'RAUL ARAYA', paciente: 'CARMEN ISLA', rut: '10112348-3', prevision: 'ISAPRE' },
     { hora: '10:30', especialista: 'MARIA ARRIAGADA', paciente: 'ANA KLAPP', rut: '17879423-9', prevision: 'ISAPRE' },
-    { hora: '10:30', especialista: 'ANTONIO LARENAS', paciente: 'PABLO LOAYZA', rut: '13453234-1', prevision: 'ISAPRE' },
     { hora: '11:00', especialista: 'ALEJANDRO BADILLA', paciente: 'FELIPE MARDONES', rut: '1547423-6', prevision: 'ISAPRE' },
     { hora: '11:30', especialista: 'CECILIA BUDNIK', paciente: 'DIEGO MARRE', rut: '16554741-K', prevision: 'FONASA' },
     { hora: '12:00', especialista: 'ARTURO CAVAGNARO', paciente: 'CECILIA MENDEZ', rut: '9747535-8', prevision: 'ISAPRE' },
-    { hora: '12:00', especialista: 'MATIAS ARAVENA', paciente: 'SUSANA POBLETE', rut: '14345656-6', prevision: 'FONASA' },
     { hora: '12:30', especialista: 'ANDRES KANACRI', paciente: 'MARCIAL SUAZO', rut: '11254785-5', prevision: 'ISAPRE' },
 ];
 
@@ -31,6 +26,16 @@ const dental = [
     { hora: '13:30', especialista: 'EDUARDO VIÑUELA', paciente: 'HUGO SANCHEZ', rut: '17665461-4', prevision: 'FONASA' },
     { hora: '14:00', especialista: 'RAQUEL VILLASECA', paciente: 'ANA SEPULVEDA', rut: '14441281-0', prevision: 'ISAPRE' },
 ];
+
+//punto 1 - agregar lineas
+
+traumatologia.push(
+    { hora: '9:00', especialista: 'RENÉ POBLETE', paciente: 'ANA GELLONA', rut: '13123329-7', prevision: 'ISAPRE' },
+    { hora: '9:30', especialista: 'MARIA SOLAR', paciente: 'RAMIRO ANDRADE', rut: '12221451-K', prevision: 'FONASA' },
+    { hora: '10:00', especialista: 'RAUL LOYOLA', paciente: 'CARMEN ISLA', rut: '10112348-3', prevision: 'ISAPRE' },
+    { hora: '10:30', especialista: 'ANTONIO LARENAS', paciente: 'PABLO LOAYZA', rut: '13453234-1', prevision: 'ISAPRE' },
+    { hora: '12:00', especialista: 'MATIAS ARAVENA', paciente: 'SUSANA POBLETE', rut: '14345656-6', prevision: 'FONASA' },
+)
 
 //punto 2 - eliminar primer y ultimo elemento de radiologia
 radiologia.shift();
@@ -49,15 +54,16 @@ mostrarCitasEnHTML(dental);
 
 //punto 4 - listado de todos los pacientes.
 document.write('<h2>Pacientes del centro medico</h2>');
-document.write('<h3>Nombre Apellido</h3>');
+document.write('<h3>Nombre y Apellido</h3>');
+
+const listapaciente= radiologia.concat(traumatologia, dental);
+
 function mostrarNombres(citas) {
     citas.forEach(cita => {
         document.write(`<p>${cita.paciente}</p>`);
     });
 }
-mostrarNombres(radiologia);
-mostrarNombres(traumatologia);
-mostrarNombres(dental);
+mostrarNombres(listapaciente);
 
 //punto 5 - pasiente Isapre en Dental
 
